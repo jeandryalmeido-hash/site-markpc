f93cb47c13c276b97b868de6d315f958a2306a6c
mark <jeandryalmeido@gmail.com>
2026-03-24 17:06:41 -0300

    corrige listar retorno

--- api/listar.js
+++ api/listar.js
@@ -10,10 +10,7 @@ module.exports = async function handler(req, res) {
       .sort({ _id: -1 })
       .toArray();
 
-    return res.status(200).json({
-      ok: true,
-      produtos
-    });
+    return res.status(200).json(produtos);
   } catch (err) {
     return res.status(500).json({
       ok: false,
